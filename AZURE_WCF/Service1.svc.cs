53ee954fc5a4c1b0163bc464d84d48d87212736d
Your Name <you@example.com>
2016-06-25 22:40:41 +0200

    another

--- AZURE_WCF/Service1.svc.cs
+++ AZURE_WCF/Service1.svc.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
 using System.Text;
 
@@ -10,6 +11,7 @@ namespace AZURE_WCF
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class Service1 : IService1
     {
         public string GetData()
